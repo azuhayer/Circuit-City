6a0f6f60d798483a9f07dc901ef276a2f64feaf2
sergeantshaky <shossai019@citymail.cuny.edu>
2023-04-25 00:09:42 -0400

    css and js update

--- script.js
+++ script.js
@@ -28,3 +28,13 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+// Minimized window menu tab interaction
+
+let menu = document.querySelector('#menu-icon');
+let navmenu = document.querySelector('.navmenu');
+
+menu.onclick = () => {
+  menu.classList.toggle('bx-x');
+  navmenu.classList.toggle('open');
+}
