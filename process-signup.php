f0e385ce3114e8f932af857d051ac6d0fd5f3f83
Zuhayer Alvi <azuhayer@gmail.com>
2023-05-02 11:21:28 -0400

    Update process-signup.php

--- process-signup.php
+++ process-signup.php
@@ -10,4 +10,19 @@ if (! filter_var($_POST["email"], FILTER_VALIDATE_EMAIL))
     die("Valid email is required.");
 }
 
+if(strlen($_POST["password"]) < 8)
+{
+    die("Password must be at least 8 characters.");
+}
+
+if(! preg_match("/[a-z]/i", $_POST["password"]))
+{
+    die("Password must contain at least one letter");
+}
+
+if(! preg_match("/[0-9]/i", $_POST["password"]))
+{
+    die("Password must contain at least one number");
+}
+
 print_r($_POST);
