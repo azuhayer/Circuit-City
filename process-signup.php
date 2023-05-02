c33ad9e5c8c0b2374931d395261c10d636c91af3
Zuhayer Alvi <azuhayer@gmail.com>
2023-05-02 00:45:56 -0400

    Update process-signup.php

--- process-signup.php
+++ process-signup.php
@@ -5,4 +5,9 @@ if(empty($_POST["name"]))
     die("Name is required.");
 }
 
+if (! filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) 
+{
+    die("Valid email is required.");
+}
+
 print_r($_POST);
