f35c9e9bdc5bdd8d649ccaca86ce71759e1d5994
Zuhayer Alvi <azuhayer@gmail.com>
2023-05-03 17:39:07 -0400

    Update script.js

--- script.js
+++ script.js
@@ -6,23 +6,18 @@ window.addEventListener("scroll", function(){
 })
 
 /* SIGNUP MODAL JAVASCRIPT */
-var modal = document.getElementById("signup-modal");            // Get the modal element
+var modal = document.getElementById("signup-modal");            
+var userIcon = document.getElementById("user-icon");            
+var closeButton = document.getElementsByClassName("close")[0];  
 
-var userIcon = document.getElementById("user-icon");            // Get the user icon element
-
-var closeButton = document.getElementsByClassName("close")[0];  // Get the close button element
-
-// When the user clicks on the user icon, show the modal
 userIcon.onclick = function() { 
   modal.style.display = "block";
 }
 
-// When the user clicks on the close button, hide the modal
 closeButton.onclick = function() {
   modal.style.display = "none";
 }
 
-// When the user clicks outside of the modal, hide it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
