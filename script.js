af1c88492e4daf106bcda5e13009dede94d05d50
Zuhayer Alvi <azuhayer@gmail.com>
2023-05-12 13:02:22 -0400

    new build updates [WIP]

--- script.js
+++ script.js
@@ -158,3 +158,183 @@ function updatetotal(){
     total = Math.round(total * 100) / 100;
     document.getElementsByClassName("total-price")[0].innerText = "$" + total;
 }
+
+
+
+// NEW BUILD JAVASCRIPT [WORK IN PROGRESS]
+var components = [
+  {
+    name: "CPU",
+    brand: "Brand 1",
+    price: 200,
+    image: "cpu.png"  //change image
+  },
+  {
+    name: "CPU COOLER",
+    brand: "Brand 1",
+    price: 200,
+    image: "cpucooler.png"  //change image
+  },
+  {
+    name: "MOTHERBOARD",
+    brand: "Brand 1",
+    price: 200,
+    image: "cpucooler.png"  //change image
+  },
+  {
+    name: "MEMORY",
+    brand: "Brand 1",
+    price: 200,
+    image: "cpucooler.png"  //change image
+  },
+  {
+    name: "STORAGE",
+    brand: "Brand 2",
+    price: 400,
+    image: "gpu.png"  //change image
+  },
+  {
+    name: "GRAPHICS CARD",
+    brand: "Brand 1",
+    price: 200,
+    image: "cpucooler.png"  //change image
+  },
+  {
+    name: "CASE",
+    brand: "Brand 1",
+    price: 200,
+    image: "cpucooler.png"  //change image
+  },
+  {
+    name: "POWER SUPPLY",
+    brand: "Brand 1",
+    price: 200,
+    image: "cpucooler.png"  //change image
+  },
+  // Add more component objects as needed
+];
+
+// Display the initial component list
+function displayComponents() {
+  var componentList = document.getElementById("component-list");
+
+  // Clear the existing component list
+  componentList.innerHTML = "";
+
+  // Iterate over the components and create component cards
+  components.forEach(function (component) {
+      var card = document.createElement("div");
+      card.className = "component-card";
+
+      var image = document.createElement("img");
+      image.src = component.image;
+      card.appendChild(image);
+
+      var name = document.createElement("h3");
+      name.textContent = component.name;
+      card.appendChild(name);
+
+      var brand = document.createElement("p");
+      brand.textContent = "Brand: " + component.brand;
+      card.appendChild(brand);
+
+      var price = document.createElement("p");
+      price.textContent = "Price: $" + component.price;
+      card.appendChild(price);
+
+      // Add a button to add the component to the build list
+      var addButton = document.createElement("button");
+      addButton.textContent = "Add to Build";
+      addButton.addEventListener("click", function () {
+          addToBuildList(component);
+      });
+      card.appendChild(addButton);
+
+      componentList.appendChild(card);
+  });
+}
+
+// Add a component to the build list
+function addToBuildList(component) {
+  var buildList = document.getElementById("build-items");
+
+  var listItem = document.createElement("li");
+  listItem.textContent = component.name + " ($" + component.price + ")";
+
+  buildList.appendChild(listItem);
+
+  // Update the total cost
+  updateTotalCost(component.price);
+}
+
+// Update the total cost
+function updateTotalCost(price) {
+  var totalCost = document.getElementById("total-cost");
+  var currentCost = parseInt(totalCost.textContent.substring(1)); // Remove the dollar sign and parse the integer
+
+  var newCost = currentCost + price;
+  totalCost.textContent = "$" + newCost;
+}
+
+// Filter the component list based on the selected brand
+function filterComponentsByBrand() {
+  var brandFilter = document.getElementById("brand-filter");
+  var selectedBrand = brandFilter.value;
+
+  // If no brand is selected, show all components
+  if (!selectedBrand) {
+      displayComponents();
+      return;
+  }
+
+  // Filter the components based on the selected brand
+  var filteredComponents = components.filter(function (component) {
+      return component.brand === selectedBrand;
+  });
+
+  // Display the filtered components
+  var componentList = document.getElementById("component-list");
+  componentList.innerHTML = "";
+  filteredComponents.forEach(function (component) {
+      var card = createComponentCard(component);
+      componentList.appendChild(card);
+  });
+}
+
+// Create a component card
+function createComponentCard(component) {
+  var card = document.createElement("div");
+  card.className = "component-card";
+
+  var image = document.createElement("img");
+  image.src = component.image;
+  card.appendChild(image);
+
+  var name = document.createElement("h3");
+  name.textContent = component.name;
+  card.appendChild(name);
+
+  var brand = document.createElement("p");
+  brand.textContent = "Brand: " + component.brand;
+  card.appendChild(brand);
+
+  var price = document.createElement("p");
+  price.textContent = "Price: $" + component.price;
+  card.appendChild(price);
+
+  var addButton = document.createElement("button");
+  addButton.textContent = "Add to Build";
+  addButton.addEventListener("click", function () {
+      addToBuildList(component);
+  });
+  card.appendChild(addButton);
+
+  return card;
+}
+
+// Event listener for brand filter changes
+var brandFilter = document.getElementById("brand-filter");
+brandFilter.addEventListener("change", filterComponentsByBrand);
+
+// Display the initial component list
+displayComponents();
